5a5ed34908f0c21b9be9c703dd7eaa8a1ed312cd
PinAI <pinaiwork777@gmail.com>
2017-04-12 17:45:22 +0300

    RSA digital signature

---
+++ encryption/model/algorithms/DigitalSignature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using encryption.model.AsymmetricEncryption;
+using System.Numerics;
+
+namespace encryption.model.DigitalSignature
+{
+    public static class DigitalSignature
+    {
+        //проверка электронной подписи RSA
+        public static bool CheckRSASign(string source, BigInteger sign, Tuple<BigInteger, BigInteger> openKey, BigInteger n)
+        {
+            BigInteger hash = GetHash(source, n);
+            BigInteger encS = AsymmetricEncryption.AsymmetricEncryption.RSADecrypt(sign, openKey);
+            if (hash == encS) return true;
+            else return false;
+        }
+
+        //создание электронной подписи RSA
+        public static Tuple<string, BigInteger> MakeRSASign(string source, Tuple<BigInteger, BigInteger> closeKey, BigInteger n)
+        {
+            BigInteger hash = GetHash(source, n);
+            BigInteger result = AsymmetricEncryption.AsymmetricEncryption.RSAEncrypt(hash, closeKey);
+            return new Tuple<string, BigInteger>(source, result);
+        }
+
+        //хеш функция
+        public static BigInteger GetHash(string s, BigInteger n)
+        {
+            BigInteger result = 150;
+            for (int i = 0; i < s.Length; i++)
+            {                
+                result = BigInteger.ModPow(BigInteger.Add(result, new BigInteger(s[i])), 2, n);
+            }
+
+            return result;
+        }
+    }
+}
